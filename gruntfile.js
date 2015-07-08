8b8807423def93f5aab2e28cb4c216eb0885a0a8
jianghai <jianghai1007@gmail.com>
2015-07-08 11:24:24 +0800

    remove

--- gruntfile.js
+++ gruntfile.js
@@ -1,29 +1,28 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: 
-                    '/**\n' +
-                    ' * <%= pkg.name %>.min.js \n' +
-                    ' * http://github.com/jianghai/<%= pkg.name %>\n' +
-                    ' */\n'
-            },
-            build: {
-                src: '<%= pkg.name %>.js',
-                dest: '<%= pkg.name %>.min.js'
-            }
-        }
-    });
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/**\n' +
+          ' * <%= pkg.name %>.min.js \n' +
+          ' * http://github.com/jianghai/<%= pkg.name %>\n' +
+          ' */\n'
+      },
+      build: {
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.min.js'
+      }
+    }
+  });
 
-    // These plugins provide necessary tasks.
-    require('load-grunt-tasks')(grunt, {
-        scope: 'devDependencies'
-    });
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt, {
+    scope: 'devDependencies'
+  });
 
-    // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
 
 };
